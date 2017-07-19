9d8301288276c581a7b956a8f78bdbd688ce799f
kyo <kunal17058@gmail.com>
2017-07-19 14:26:16 +0530

    iiitapp

---
+++ eventdetails.php
@@ -0,0 +1,18 @@
+{
+"error":false,
+"eventdetail":[
+{
+  "event":"Internet of Things (IoT) and Arduino Workshop",
+  "date":"4th December 2017",
+  "time":"10:00 AM",
+  "details":"2 Days of IoT workshop has been arranged in IIIT Nagpur. Any inersted Students can register.\n\nFee:1500/- \n\nContact: XYZ\t9123456789"
+},
+{
+  "event":"Tree Plantation Drive",
+  "date":"23 Oct 2017",
+  "time":"04:00 PM",
+  "details":"Tree Days of IoT workshop has been arranged in IIIT Nagpur. Any inersted Students can register.\n\nFee:1500/- \n\nContact: XYZ\t9123456789"
+}
+]
+
+}
---
+++ events.php
@@ -0,0 +1,31 @@
+{
+"error":false,
+"events":[
+{
+  "event":"Internet of Things (IoT) and Arduino Workshop",
+  "date":"4th December 2017",
+  "time":"10:00 AM"
+},
+{
+  "event":"Tree Plantation Drive",
+  "date":"23th Oct 2017",
+  "time":"04:00 PM"
+},
+{
+  "event":"Yoga Day",
+  "date":"21th June 2017",
+  "time":"06:00 AM"
+},
+{
+  "event":"Abhiyakti 2K17",
+  "date":"11th May 2017",
+  "time":"10:30 AM"
+},
+{
+  "event":"Institue Gathering 2K17",
+  "date":"22th March 2017",
+  "time":"09:00 AM"
+}
+]
+
+}
